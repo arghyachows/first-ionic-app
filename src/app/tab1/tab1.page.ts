c15662f1cd1330dc33d516e68f886953a703f751
Arghya Chowdhury <arghyachowdhury2610@gmail.com>
2020-02-27 00:55:23 +0530

    Menu for Courses

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Course } from '../types';
 import { CoursesService } from '../courses.service';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -11,8 +12,41 @@ import { CoursesService } from '../courses.service';
 export class Tab1Page {
 
   courseList: Observable<Course[]>;
-  constructor(private courseService: CoursesService) {
+  constructor(private courseService: CoursesService, private actionSheetController: ActionSheetController) {
     this.courseList = courseService.getAllCourses();
+  }
+
+  async presentActionSheet(course: Course) {
+    const actionSheet = await this.actionSheetController.create({
+      header: course.title,
+      buttons: [{
+        text: 'Play',
+        icon: 'play',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
